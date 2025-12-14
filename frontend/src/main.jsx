f78d6f0b32720ac28f213bb4d0c33c9e05f27680
baronocasiones <baronocasiones@gmail.com>
2025-12-14 08:31:05 +0800

    started lobby

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import Landing from './pages/Landing.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+    { path: '/', element: <Landing /> },
+    { path: '/lobby', element: <h1>Lobby Page</h1> }
+])
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <Landing />
-    </StrictMode>,
+        <RouterProvider router={router} />
+    </StrictMode >,
 )
