ed2fb6ae7d22a4827a44161132267dc533d5dde3
baronocasiones <baronocasiones@gmail.com>
2025-12-19 14:34:21 +0800

    frontend done

--- frontend/src/pages/PlayRoom.jsx
+++ frontend/src/pages/PlayRoom.jsx
@@ -1,5 +1,6 @@
 import styles from './../styles/PlayRoom.module.css'
 import PrimaryButton from './../components/PrimaryButton.jsx'
+import ChatBox from './../components/ChatBox.jsx'
 import { useEffect } from 'react'
 
 function PlayRoom() {
@@ -15,85 +16,113 @@ function PlayRoom() {
 
     return (
         <>
-            <div className={styles.header}>
-                <div className={styles.currentTurnContainer}>
-                    <label style={{ color: '#A8B2D1' }}>Current Turn </label>
-                    <span style={{ color: '#E94560' }}>Alexandra's Turn</span>
+            <div>
+                <div className={styles.header}>
+                    <div className={styles.currentTurnContainer}>
+                        <label style={{ color: '#A8B2D1' }}>Current Turn </label>
+                        <span style={{ color: '#E94560' }}>Alexandra's Turn</span>
+                    </div>
+                    <div className={styles.statsContainer}>
+                        <div>
+                            <label className={styles.statLabels}>Round </label><br />
+                            <span>3</span>
+                        </div>
+                        <div>
+                            <label className={styles.statLabels}>Players Left </label><br />
+                            <span>4/4</span>
+                        </div>
+                        <div>
+                            <label className={styles.statLabels}>Treasury </label><br />
+                            <span>50</span>
+                        </div>
+                    </div>
+                    <PrimaryButton text='Menu' backgroundColor='rgba(255, 255, 255, 0.098)' width='auto' />
                 </div>
-                <div className={styles.statsContainer}>
-                    <div>
-                        <label className={styles.statLabels}>Round </label><br />
-                        <span>3</span>
+                <div className={styles.mainContainer}>
+                    <div className={styles.gameContainer}>
+                        <div className={styles.playersContainer}>
+                            <div className={styles.player}>
+                                <span className={styles.profilePic}></span>
+                                <span className={styles.playerName}>Marcus</span>
+                                <div className={styles.coins}>
+                                    <span className={styles.coinIcon}></span>
+                                    <span className={styles.coinValue}>5</span>
+                                </div>
+                                <div className={styles.cardsContainer}>
+                                    <span className={styles.card}></span>
+                                    <span className={styles.card}></span>
+                                </div>
+                            </div>
+                            <div className={styles.player}>
+                                <span className={styles.profilePic}></span>
+                                <span className={styles.playerName}>Sophia</span>
+                                <div className={styles.coins}>
+                                    <span className={styles.coinIcon}></span>
+                                    <span className={styles.coinValue}>3</span>
+                                </div>
+                                <div className={styles.cardsContainer}>
+                                    <span className={styles.card}></span>
+                                    <span className={styles.card}></span>
+                                </div>
+                            </div>
+                            <div className={styles.player}>
+                                <span className={styles.profilePic}></span>
+                                <span className={styles.playerName}>James</span>
+                                <div className={styles.coins}>
+                                    <span className={styles.coinIcon}></span>
+                                    <span className={styles.coinValue}>7</span>
+                                </div>
+                                <div className={styles.cardsContainer}>
+                                    <span className={styles.card}></span>
+                                    <span className={styles.card}></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.movePreview}>
+                            <h3 style={{ textAlign: 'center' }}>Marcus claims to be the Duke</h3>
+                            <span style={{ textAlign: 'center', color: '#A8B2D1' }}>Marcus is taking 3 coins from the treasury. You can challenge this claim or let it pass.</span>
+                            <div className={styles.challengeButton}>
+                                <PrimaryButton text='Challenge' width='auto' />
+                                <PrimaryButton text='Pass' backgroundColor='rgba(255, 255, 255, 0.098)' width='auto' />
+                            </div>
+                        </div>
                     </div>
                     <div>
-                        <label className={styles.statLabels}>Players Left </label><br />
-                        <span>4/4</span>
-                    </div>
-                    <div>
-                        <label className={styles.statLabels}>Treasury </label><br />
-                        <span>50</span>
+                        <ChatBox header="Game Log" withSubmission={false} />
                     </div>
                 </div>
-                <PrimaryButton text='Menu' backgroundColor='rgba(255, 255, 255, 0.098)' width='auto' />
             </div>
-            <div className={styles.mainContainer}>
-                <div className={styles.gameContainer}>
-                    <div className={styles.playersContainer}>
-                        <div className={styles.player}>
-                            <span className={styles.profilePic}></span>
-                            <span className={styles.playerName}>Marcus</span>
-                            <div className={styles.coins}>
-                                <span className={styles.coinIcon}></span>
-                                <span className={styles.coinValue}>5</span>
-                            </div>
-                            <div className={styles.cardsContainer}>
-                                <span className={styles.card}></span>
-                                <span className={styles.card}></span>
-                            </div>
-                        </div>
-                        <div className={styles.player}>
-                            <span className={styles.profilePic}></span>
-                            <span className={styles.playerName}>Sophia</span>
-                            <div className={styles.coins}>
-                                <span className={styles.coinIcon}></span>
-                                <span className={styles.coinValue}>3</span>
-                            </div>
-                            <div className={styles.cardsContainer}>
-                                <span className={styles.card}></span>
-                                <span className={styles.card}></span>
-                            </div>
-                        </div>
-                        <div className={styles.player}>
-                            <span className={styles.profilePic}></span>
-                            <span className={styles.playerName}>James</span>
-                            <div className={styles.coins}>
-                                <span className={styles.coinIcon}></span>
-                                <span className={styles.coinValue}>7</span>
-                            </div>
-                            <div className={styles.cardsContainer}>
-                                <span className={styles.card}></span>
-                                <span className={styles.card}></span>
-                            </div>
-                        </div>
+            <div className={styles.userUIContainer}>
+                <div className={styles.userInfo}>
+                    <div className={styles.userIdentifier}>
+                        <span className={styles.profilePic} style={{ backgroundColor: '#E94560', width: '56px', height: '56px' }}></span>
+                        <h3 style={{ alignContent: 'center' }}>You (Alexandra)</h3>
                     </div>
-                    <div className={styles.movePreview}>
-                        <h3 style={{ textAlign: 'center' }}>Marcus claims to be the Duke</h3>
-                        <span style={{ textAlign: 'center', color: '#A8B2D1' }}>Marcus is taking 3 coins from the treasury. You can challenge this claim or let it pass.</span>
-                        <div className={styles.challengeButton}>
-                            <PrimaryButton text='Challenge' width='auto' />
-                            <PrimaryButton text='Pass' backgroundColor='rgba(255, 255, 255, 0.098)' width='auto' />
-                        </div>
+                    <div className={styles.userCoins}>
+                        <span style={{ width: '28px', height: '28px', borderRadius: '100%', display: 'inline-block', backgroundColor: '#FFD700' }}></span>
+                        <span style={{ color: '#FFD700' }}>4</span>
                     </div>
-                    <div className={styles.userUIContainer}>
-                        <div className={styles.userInfo}>
-                            <div className={styles.userIdentifier}>
-                                <span className={styles.profilePic} style={{ backgroundColor: '#E94560', width: '56px', height: '56px' }}></span>
-                                <h3 style={{ alignContent: 'center' }}>You (Alexandra)</h3>
-                            </div>
-                            <div className={styles.userCoins}>
-                                <span style={{ width: '28px', height: '28px', borderRadius: '100%', display: 'inline-block', backgroundColor: '#FFD700'}}></span>
-                                <span style={{ color: '#FFD700'}}>4</span>
-                            </div>
+                </div>
+                <div className={styles.userCardsContainer}>
+                    <span className={styles.userCard}>
+                        <h4>DUKE</h4>
+                        <label>Take 3 coins</label>
+                    </span>
+                    <span className={styles.userCard}>
+                        <h4>ASSASSIN</h4>
+                        <label>Pay 3 to eliminate</label>
+                    </span>
+                    <div className={styles.userMoves}>
+                        <div style={{ display: 'flex', columnGap: '12px', justifyContent: 'center' }}>
+                            <PrimaryButton text='Income' backgroundColor='rgba(255, 255, 255, 0.098)' width='auto' />
+                            <PrimaryButton text='Foreign Aid' backgroundColor='rgba(255, 255, 255, 0.098)' width='auto' />
+                            <PrimaryButton text='Coup (7)' width='auto' />
+                        </div>
+                        <div style={{ display: 'flex', columnGap: '12px', justifyContent: 'center' }}>
+                            <PrimaryButton text='Tax (Duke)' backgroundColor='rgba(255, 255, 255, 0.098)' width='auto' />
+                            <PrimaryButton text='Assassinate (Assassin)' backgroundColor='rgba(255, 255, 255, 0.098)' width='auto' />
+                            <PrimaryButton text='Steal (Captain)' backgroundColor='rgba(255, 255, 255, 0.098)' width='auto' />
+                            <PrimaryButton text='Exchange (Ambassador)' backgroundColor='rgba(255, 255, 255, 0.098)' width='auto' />
                         </div>
                     </div>
                 </div>
